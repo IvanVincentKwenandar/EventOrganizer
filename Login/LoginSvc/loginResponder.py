e0a563e148064c17c9153e357efd1448d7a2d306
IvanVincentKwenandar <c14200138@john.petra.ac.id>
2023-08-22 17:59:08 +0800

    Push

---
+++ Login/LoginSvc/loginResponder.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, Response as HTTPResponse, request as HTTPRequest, jsonify
+import mysql.connector
+import json
+from flask_cors import CORS
+
+db = mysql.connector.connect(host="LoginSQL", user="root", password="root",database="loginservice")
+cursor = db.cursor(dictionary=True)
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/eo/login', methods = ['POST', 'GET'])
+def login():
+    db = mysql.connector.connect(host="LoginSQL", user="root", password="root",database="loginservice")
+    cursor = db.cursor(dictionary=True)
+    jsonData = ''
+    statusCode = 400
+    userInfo = json.loads(HTTPRequest.data)
+    sql = "SELECT * FROM usertable WHERE username = %s AND password = %s"
+    cursor.execute(sql,[userInfo['username'],userInfo['password']])
+    data = cursor.fetchall()
+    jsonData = json.dumps(data)
+    statusCode = 200
+    resp = HTTPResponse()
+    if jsonData !='': resp.response = jsonData
+    resp.headers['Content-Type'] = 'application/json'
+    resp.status = statusCode
